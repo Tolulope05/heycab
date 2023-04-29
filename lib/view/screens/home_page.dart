ddb4ec0edce192f66c6b5e025b7e916dece54b88
Tolulope Fakunle <fakunletolulope05@gmail.com>
2023-04-29 05:56:27 +0100

    did portfolio section

--- lib/view/screens/home_page.dart
+++ lib/view/screens/home_page.dart
@@ -67,7 +67,108 @@ class HomePage extends ConsumerWidget {
                 )
               ],
             ),
-            Container(),
+            Row(
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(
+                    bottom: 10,
+                    top: 10,
+                    right: 10,
+                    left: 10,
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 15,
+                    horizontal: 25,
+                  ),
+                  decoration: BoxDecoration(
+                    color: const Color(0xff1D1B1E),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Column(
+                    children: [
+                      Container(
+                        constraints: BoxConstraints.tight(const Size(470, 50)),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              "UI Portfolio",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w700,
+                                fontSize: Theme.of(context)
+                                        .textTheme
+                                        .bodyMedium!
+                                        .fontSize! *
+                                    1.1,
+                              ),
+                            ),
+                            const Expanded(
+                                child:
+                                    SizedBox()), // Reason I gave parent constraint to the row
+                            Text(
+                              "See All",
+                              style: TextStyle(
+                                color: const Color(0xFFA2A2A2),
+                                fontWeight: FontWeight.w700,
+                                fontSize: Theme.of(context)
+                                        .textTheme
+                                        .bodyMedium!
+                                        .fontSize! *
+                                    1.1,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Image.asset(
+                              "assets/images/portfolio-one.jpg",
+                              width: 150,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Image.asset(
+                              "assets/images/portfolio-two.jpg",
+                              width: 150,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Image.asset(
+                              "assets/images/portfolio-three.jpg",
+                              width: 150,
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(
+                    bottom: 10,
+                    top: 10,
+                    right: 10,
+                    left: 10,
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 15,
+                    horizontal: 25,
+                  ),
+                  decoration: BoxDecoration(
+                    color: const Color(0xff1D1B1E),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),
--- lib/view/widgets/exp_card.dart
+++ lib/view/widgets/exp_card.dart
@@ -16,34 +16,35 @@ class ExperienceCarrd extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        height: 150,
-        width: 140,
-        margin: const EdgeInsets.all(5),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-          color: bgColor,
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              "$years +",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: textColor,
-                fontSize: Theme.of(context).textTheme.headlineSmall!.fontSize,
-              ),
+      height: 150,
+      width: 140,
+      margin: const EdgeInsets.all(5),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: bgColor,
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            "$years +",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: textColor,
+              fontSize: Theme.of(context).textTheme.headlineSmall!.fontSize,
             ),
-            Text(
-              title,
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: textColor,
-                fontSize: Theme.of(context).textTheme.bodySmall!.fontSize,
-              ),
+          ),
+          Text(
+            title,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: textColor,
+              fontSize: Theme.of(context).textTheme.bodySmall!.fontSize,
             ),
-          ],
-        ));
+          ),
+        ],
+      ),
+    );
   }
 }
